d33e0e0da1a893709745533f78a7a3799f314a5e
Keith Lowery <klowery@amd.com>
2024-10-31 12:21:05 -0500

    Improvements to message_logger output

--- inc/message_logger.h
+++ inc/message_logger.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <map>
+#include <iostream>
+#include <fstream>
 #include "message_handlers.h"
 
 class message_logger_t : public dh_comms::message_handler_base
 {
 public:
-    message_logger_t(const std::string& strKernel, uint64_t dispatch_id, bool verbose = false);
+    message_logger_t(const std::string& strKernel, uint64_t dispatch_id, std::string& location, bool verbose = false);
     message_logger_t(const message_logger_t&) = default;
     virtual ~message_logger_t();
     virtual bool handle(const dh_comms::message_t &message) override;
@@ -16,7 +18,9 @@ public:
 private:
     std::string strKernel_;
     uint64_t dispatch_id_;
+    std::string location_;
     bool verbose_;
+    std::ostream *log_file_;
     // out iostream here
 };
 
--- src/message_logger.cc
+++ src/message_logger.cc
@@ -1,20 +1,29 @@
 #include <cstdio>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <cassert>
 #include "inc/message_logger.h"
 #include "data_headers.h"
 #include "message.h"
 
-message_logger_t::message_logger_t(const std::string& strKernel, uint64_t dispatch_id, bool verbose)
+message_logger_t::message_logger_t(const std::string& strKernel, uint64_t dispatch_id, std::string& location, bool verbose)
     : strKernel_(strKernel),
       dispatch_id_(dispatch_id),
+      location_(location),
       verbose_(verbose)
 {
+    location_ = location;
+    if (location == "console")
+        log_file_ = &std::cout;
+    else
+        log_file_ = new std::ofstream(location, std::ios::app);
 }
 
 message_logger_t::~message_logger_t()
 {
+    if(location_ != "console")
+        delete log_file_;
 }
 
 bool message_logger_t::handle(const dh_comms::message_t &message)
@@ -28,6 +37,13 @@ bool message_logger_t::handle(const dh_comms::message_t &message)
         return false;
     }
     assert(message.data_item_size() == sizeof(uint64_t));
+
+    dh_comms::wave_header_t hdr = message.wave_header();
+
+
+    *log_file_ << "\"" << strKernel_ << "\"," << std::dec << dispatch_id_ << ",";
+
+    *log_file_ << "0x" << std::hex << std::setw(sizeof(void*) * 2) << std::setfill('0') << message.wave_header().exec << ",";
     for (size_t i = 0; i != message.no_data_items(); ++i)
     {
         uint64_t address = *(const uint64_t *)message.data_item(i);
@@ -35,6 +51,12 @@ bool message_logger_t::handle(const dh_comms::message_t &message)
         {
             //printf("memory_heatmap: added address 0x%lx to map\n", address);
         }
+
+        *log_file_ << "0x" << std::hex << std::setw(sizeof(void*) * 2) << std::setfill('0') << address;
+        if (i < message.no_data_items() - 1)
+            *log_file_ << ",";
+        else
+            *log_file_ << std::endl;
     }
     return true;
 }
