a4be6d4edc2cc631bc0171453ecac6de1e03fe58
Keith Lowery <klowery@amd.com>
2024-11-05 13:47:32 -0600

    Added support for the code object version in the dh_comms bitcode file names

--- src/test/st.sh
+++ src/test/st.sh
@@ -1,3 +1,5 @@
-#hipcc  -I/opt/rocm/include -I/opt/rocm/include/roctracer -g stresstest.cc -L/opt/rocm/roctracer/lib  -v -o stresstest
-#hipcc -v -fpass-plugin=/work1/amd/klowery/instrument-amdgpu-kernels/build/lib/libAMDGCNMemTrace.so -I/opt/rocm/include -I/opt/rocm/include/roctracer -g quicktest.cc -L/opt/rocm/roctracer/lib  -v -o quicktest
-hipcc  -I/opt/rocm/include -I/opt/rocm/include/roctracer -g quicktest.cc -L/opt/rocm/roctracer/lib  -v -o quicktest
+#hipcc  -I/opt/rocm/include -I/opt/rocm/include/roctracer -g stresstest.cc -L/opt/rocm/roctracer/lib  -v -o stresstest^
+echo $PATH
+#AMDCGN_INSTRUMENTATION_FUNCTIONS_FILE=/work1/amd/klowery/instrument-amdgpu-kernels/dh_comms/build/lib/hip/dh_comms_dev.gfx90a.bc hipcc -fpass-plugin=/work1/amd/klowery/instrument-amdgpu-kernels/build/hip-static/libAMDGCNMemTrace.so -I/opt/rocm/include -I/opt/rocm/include/roctracer  -O3 quicktest.cc -L/opt/rocm/roctracer/lib  -v -o quicktest
+hipcc -fpass-plugin=/work1/amd/klowery/instrument-amdgpu-kernels/build/lib/libAMDGCNMemTrace.so -I/opt/rocm/include -I/opt/rocm/include/roctracer  -O3 quicktest.cc -L/opt/rocm/roctracer/lib  -v -o quicktest
+#hipcc  -I/opt/rocm/include -I/opt/rocm/include/roctracer -g quicktest.cc -L/opt/rocm/roctracer/lib  -v -o quicktest
