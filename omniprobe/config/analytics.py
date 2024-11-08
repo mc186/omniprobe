6d52484ef8090476cd7021007543e858ac682495
Keith Lowery <klowery@amd.com>
2024-11-08 09:07:27 -0600

    Added support for human readable names for message handler specification

---
+++ omniprobe/config/analytics.py
@@ -0,0 +1,12 @@
+[
+    {
+        "name": "MessageLogger",
+        "description": "Log raw instrumentation messages",
+        "lib_name": "libLogMessages64.so",
+    },
+    {
+        "name": "Heatmap",
+        "description": "Produce a per dispatch memory heatmap",
+        "lib_name": "libdefaultMessageHandlers64.so"
+    }
+]
