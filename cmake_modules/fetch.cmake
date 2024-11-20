0ab494d6a95ec0fbae528e57d0367caa8a2783da
coleramos425 <colramos@amd.com>
2024-11-20 11:08:22 -0600

    Target link library with path to shared object
    Signed-off-by: coleramos425 <colramos@amd.com>

--- cmake_modules/fetch.cmake
+++ cmake_modules/fetch.cmake
@@ -2,15 +2,15 @@ include(FetchContent)
 
 function(fetch_project)
   cmake_parse_arguments(FETCH_SOURCE
-    "INCLUDE_DIRS;EXCLUDE_EXAMPLES"   # options
+    "INCLUDE_DIRS;BUILD_NOW"   # options
     "NAME;GIT_REPOSITORY;GIT_TAG" # single-arg options
     ""             # multi-arg options
     ${ARGN}
   )
 
-  set(SOURCE ${CMAKE_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/source)
-  set(BUILD ${CMAKE_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/build)
-  set(SUBBUILD ${CMAKE_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/subbuild)
+  set(SOURCE ${CMAKE_CURRENT_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/source)
+  set(BUILD ${CMAKE_CURRENT_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/build)
+  set(SUBBUILD ${CMAKE_CURRENT_BINARY_DIR}/deps/${FETCH_SOURCE_NAME}/subbuild)
 
 #   FetchContent_Populate(${FETCH_SOURCE_NAME}
 #     URL ${FETCH_SOURCE_URL}
@@ -18,11 +18,6 @@ function(fetch_project)
 #     ${FETCH_SOURCE_MD5_COMMAND}
 #     SOURCE_DIR ${SOURCE}
 #   )
-
-  if(FETCH_SOURCE_EXCLUDE_EXAMPLES)
-    set(FETCH_SOURCE_EXCLUDE_EXAMPLES ${SOURCE}/examples)
-    message(STATUS "Excluding examples from ${FETCH_SOURCE_EXCLUDE_EXAMPLES}")
-  endif()
 
   FetchContent_Declare(${FETCH_SOURCE_NAME}
     GIT_REPOSITORY ${FETCH_SOURCE_GIT_REPOSITORY}
@@ -37,7 +32,7 @@ function(fetch_project)
   )
   FetchContent_MakeAvailable(${FETCH_SOURCE_NAME})
 
-  set("${FETCH_SOURCE_NAME}_LOCATION" ${CMAKE_BINARY_DIR}/deps/${FETCH_SOURCE_NAME} PARENT_SCOPE)
+  set("${FETCH_SOURCE_NAME}_LOCATION" ${CMAKE_CURRENT_BINARY_DIR}/deps/${FETCH_SOURCE_NAME} PARENT_SCOPE)
   message(STATUS "Fetched ${FETCH_SOURCE_NAME}")
 
   if (FETCH_SOURCE_INCLUDE_DIRS)
