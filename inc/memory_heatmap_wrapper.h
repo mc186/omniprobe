8aa8545c95376db67b09d1760cbf2ef10d82e77d
Keith Lowery <klowery@amd.com>
2025-01-22 14:04:56 -0600

    Fixes to headers

--- inc/memory_heatmap_wrapper.h
+++ inc/memory_heatmap_wrapper.h
@@ -13,7 +13,6 @@ public:
     virtual ~memory_heatmap_wrapper();
     virtual bool handle(const dh_comms::message_t &message) override;
     virtual bool handle(const dh_comms::message_t &message, const std::string& kernel, kernelDB::kernelDB& kdb) override;
-    virtual void report(const std::string& kernel_name, kernelDB::kernelDB& kdb) override;
     virtual void report() override;
     virtual void clear() override;
 
