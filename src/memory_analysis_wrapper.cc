561a16b8fc0aa7872345191d76cb0c7cdb2629b2
Rene van Oostrum <Rene.vanOostrum@amd.com>
2025-02-11 16:13:29 -0600

    Have the message handler wrapper's handle() function forward to a different overload of the handle() function of the wrappee.

--- src/memory_analysis_wrapper.cc
+++ src/memory_analysis_wrapper.cc
@@ -9,17 +9,14 @@
 #include <string>
 #include <iostream>
 
-memory_analysis_wrapper_t::memory_analysis_wrapper_t(const std::string& kernel, uint64_t dispatch_id, const std::string& location,  bool verbose) : 
+memory_analysis_wrapper_t::memory_analysis_wrapper_t(const std::string& kernel, uint64_t dispatch_id, const std::string& location,  bool verbose) :
     kernel_(kernel), dispatch_id_(dispatch_id), location_(location), wrapped_(verbose)
 {
 }
 
 bool memory_analysis_wrapper_t::handle(const dh_comms::message_t &message, const std::string& kernel, kernelDB::kernelDB& kdb)
 {
-    auto instructions = kdb.getInstructionsForLine(kernel,message.wave_header().src_loc_idx);
-    for (auto inst : instructions)
-        std::cout << inst.inst_ << std::endl;
-    return handle(message);
+    return wrapped_.handle(message, kernel, kdb);
 }
 
 
