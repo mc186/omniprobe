bff545a8c0ce92a00e151bf79492efaa8f360a88
coleramos425 <colramos@amd.com>
2025-03-19 09:30:47 -0500

    Iterate on new separated instrumentation approach, now using ExternalProject_Add to maintain separation in build env
    Signed-off-by: coleramos425 <colramos@amd.com>

--- cmake_modules/ext_proj_add.cmake
+++ cmake_modules/ext_proj_add.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 function(ext_proj_add)
     cmake_parse_arguments(EXT_PROJ_ADD 
         "INCLUDE_DIRS" #options
-        "NAME;C_COMPILER;CXX_COMPILER" # single-arg options
+        "NAME;C_COMPILER;CXX_COMPILER;BINARY_SUFFIX" # single-arg options
         "" # multi-arg options
         ${ARGN}
     )
@@ -18,23 +18,14 @@ function(ext_proj_add)
     # Add project as a subdirectory, optionally set custom compiler
     if (EXT_PROJ_ADD_C_COMPILER AND EXT_PROJ_ADD_CXX_COMPILER)
         ExternalProject_Add(
-            ${EXT_PROJ_ADD_NAME}
-            PREFIX ${CMAKE_BINARY_DIR}/external/${EXT_PROJ_ADD_NAME}
+            ${EXT_PROJ_ADD_NAME}${EXT_PROJ_ADD_BINARY_SUFFIX}
+            PREFIX ${CMAKE_BINARY_DIR}/external/${EXT_PROJ_ADD_NAME}${EXT_PROJ_ADD_BINARY_SUFFIX}
             SOURCE_DIR ${EXTERNAL_PROJ_DIR}
-            BINARY_DIR ${CMAKE_BINARY_DIR}/external/${EXT_PROJ_ADD_NAME}/build
-            CMAKE_ARGS -DTRITON_LLVM=${TRITON_LLVM} -DCMAKE_C_COMPILER=${EXT_PROJ_ADD_C_COMPILER} -DCMAKE_CXX_COMPILER=${EXT_PROJ_ADD_CXX_COMPILER}
-            INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+            BINARY_DIR ${CMAKE_BINARY_DIR}/external/${EXT_PROJ_ADD_NAME}${EXT_PROJ_ADD_BINARY_SUFFIX}/build
+            CMAKE_ARGS -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} -DCMAKE_C_COMPILER=${EXT_PROJ_ADD_C_COMPILER} -DCMAKE_CXX_COMPILER=${EXT_PROJ_ADD_CXX_COMPILER} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
             BUILD_ALWAYS ON
                 
-            # Prevent the install step from running automatically  
-            STEP_TARGETS build  
-            INSTALL_COMMAND ""
         )
-
-        install(DIRECTORY ${CMAKE_BINARY_DIR}/external/${EXT_PROJ_ADD_NAME}/build/lib/
-                TYPE LIB
-                USE_SOURCE_PERMISSIONS
-                FILES_MATCHING PATTERN "*.so")
     else()
         add_subdirectory(${EXTERNAL_PROJ_DIR})
     endif()
