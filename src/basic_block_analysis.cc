328c7eef6eab76e71ca020b6545bab048f236f72
Keith Lowery <klowery@amd.com>
2025-05-17 05:19:25 -0500

    Adding support for wave states and accumulating statistics from bb timestamps

--- src/basic_block_analysis.cc
+++ src/basic_block_analysis.cc
@@ -30,30 +30,44 @@ basic_block_analysis::~basic_block_analysis()
 
 bool basic_block_analysis::handle(const dh_comms::message_t &message, const std::string& kernel, kernelDB::kernelDB& kdb)
 {
+    bool bReturn = true;
     auto hdr = message.wave_header();
+    waveIdentifier_t wave = {hdr.block_idx_x, hdr.block_idx_y, hdr.block_idx_z, hdr.wave_num}; 
     try
     {
         auto& instructions = kdb.getInstructionsForLine(kernel, hdr.dwarf_line);
         if (instructions.size())
         {
-            if (current_block_)
+            auto wsit = wave_states_.find(wave);
+            if (wsit != wave_states_.end())
             {
-                auto it = block_timings_.find(current_block_);
-                if (it != block_timings_.end())
-                    it->second += hdr.timestamp - start_time_;
+                auto biit = block_info_.find(wsit->second.current_block_);
+                if (biit != block_info_.end())
+                {
+                    biit->second.count_++;
+                    biit->second.duration_ += hdr.timestamp - wsit->second.start_time_;
+                }
                 else
-                    block_timings_[current_block_] = hdr.timestamp - start_time_;
+                {
+                    block_info_[wsit->second.current_block_] = {hdr.timestamp - wsit->second.start_time_,1};
+                }
+                wsit->second.current_block_ = instructions[0].block_;
+                wsit->second.start_time_ = hdr.timestamp;
             }
-            current_block_ = instructions[0].block_;
-            start_time_ = hdr.timestamp;
+            else
+            {
+                wave_states_[wave] = {instructions[0].block_, hdr.timestamp, 1};
+            }
+            // Need to check for s_endpgm and clean up wave state here
         }
         for (auto inst : instructions)
             std::cout << inst.inst_ << std::endl;
     }
     catch (std::runtime_error e)
     {
+        return false;
     }
-    return handle(message);
+    return bReturn;
 }
 
 bool basic_block_analysis::handle(const dh_comms::message_t &message)
